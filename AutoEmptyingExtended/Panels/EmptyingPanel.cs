1347a8be904f48b0d9c6ea290553fcb909fe556a
Yury Shcherbakov <mexahuk61@outlook.com>
2016-02-26 06:09:03 +0300

    init UIPanel for buildings; project references fix

---
+++ AutoEmptyingExtended/Panels/EmptyingPanel.cs
@@ -0,0 +1,40 @@
+﻿using ColossalFramework;
+using ColossalFramework.UI;
+using UnityEngine;
+
+namespace AutoEmptyingExtended.Panels
+{
+    public class EmptyingPanel : UIPanel
+    {
+        internal CityServiceWorldInfoPanel ServicePanel { private get; set; }
+        
+        public override void Start()
+        {
+            base.Start();
+
+            this.backgroundSprite = "MenuPanel2";
+            this.position = new Vector3(0, 12);
+            this.width = this.parent.width;
+
+            //temp
+            this.height = 100;
+        }
+
+        internal void OnVisibilityChanged(UIComponent component, bool show)
+        {
+            //this.isVisible = false;
+            if (show)
+            {
+                var buildingId = WorldInfoPanel.GetCurrentInstanceID().Building;
+
+                //At first click buildingId == 0. Why?
+                Debug.Log($"EmptyingPanel {buildingId}");
+                var buildingAi = Singleton<BuildingManager>.instance.m_buildings.m_buffer[buildingId].Info.m_buildingAI;
+                if (buildingAi is LandfillSiteAI || buildingAi is CemeteryAI)
+                {
+                    //this.isVisible = true;
+                }
+            }
+        }
+    }
+}
---
+++ AutoEmptyingExtended/Panels/PanelLoader.cs
@@ -0,0 +1,40 @@
+﻿using ColossalFramework.UI;
+using ICities;
+using UnityEngine;
+
+namespace AutoEmptyingExtended.Panels
+{
+    public class ModLoading : LoadingExtensionBase
+    {
+        private LoadMode _mode;
+        private GameObject _bwGameObject;
+        private EmptyingPanel _emptyingPanel;
+
+        public override void OnLevelLoaded(LoadMode mode)
+        {
+            if (mode != LoadMode.LoadGame && mode != LoadMode.NewGame)
+                return;
+            _mode = mode;
+
+            _bwGameObject = new GameObject("buildingWindowObject");
+            var serviceBuildingInfo = UIView.Find<UIPanel>("(Library) CityServiceWorldInfoPanel");
+
+            _emptyingPanel = _bwGameObject.AddComponent<EmptyingPanel>();
+            _emptyingPanel.transform.parent = serviceBuildingInfo.transform;
+            _emptyingPanel.ServicePanel = serviceBuildingInfo.gameObject.transform.GetComponentInChildren<CityServiceWorldInfoPanel>();
+            serviceBuildingInfo.eventVisibilityChanged += (component, show) => _emptyingPanel.OnVisibilityChanged(component, show);
+        }
+
+
+        public override void OnLevelUnloading()
+        {
+            if (_mode != LoadMode.LoadGame && _mode != LoadMode.NewGame)
+                return;
+            
+            if (_bwGameObject != null)
+            {
+                UnityEngine.Object.Destroy(_bwGameObject);
+            }
+        }
+    }
+}
