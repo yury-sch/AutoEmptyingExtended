c96e77f0cd745ce74904719fcaa5143c4e31efc3
Yury Shcherbakov <mexahuk61@outlook.com>
2016-02-27 20:21:42 +0300

    settings position fix

--- AutoEmptyingExtended/Panels/ConfigurationPanel.cs
+++ AutoEmptyingExtended/Panels/ConfigurationPanel.cs
@@ -10,15 +10,19 @@ namespace AutoEmptyingExtended.Panels
         {
             base.Start();
 
-            this.backgroundSprite = "MenuPanel2";
-            this.position = new Vector3(0, 12);
-            this.width = 500;
-            this.height = 200;
             this.isVisible = false;
+
+            this.backgroundSprite = "MenuPanel2";
+            this.size = new Vector2(350, 300);
+            this.absolutePosition = new Vector3(GetUIView().fixedWidth - 70 - this.width, GetUIView().fixedHeight - 130 - this.height);
 
             var uiTimeRange = this.AddUIComponent<UITimeRange>();
             uiTimeRange.position = new Vector3(15, -60);
             uiTimeRange.size = new Vector2(this.width - uiTimeRange.position.x * 2, 40);
+
+            var uiTimeRange2 = this.AddUIComponent<UITimeRange>();
+            uiTimeRange2.position = new Vector3(15, uiTimeRange.position.y - uiTimeRange.size.y - 20);
+            uiTimeRange2.size = new Vector2(this.width - uiTimeRange2.position.x * 2, 40);
         }
     }
 }
