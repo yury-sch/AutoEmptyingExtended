77238c6c69cf9958bdca8fdcd482e0ac2183ad89
Vadim Lyubanenko <lubanenko@yandex.ru>
2017-05-22 20:32:28 +0300

    1.7.0-f5 compatibility added

---
+++ AutoEmptyingExtended/Utils/ModException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutoEmptyingExtended.Utils
+{
+    public class DetailedException : Exception
+    {
+        public DetailedException(string message) : base($"{message}" +
+                                                        "\n -----------------------------------------------" +
+                                                        "\n Please, notify the mod authors on SteamWorkshop" +
+                                                        "\n -----------------------------------------------") { }
+    }
+}
